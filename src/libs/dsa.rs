1ba15f8fc0b72684127a55730b419f817fc3bbb4
Arkiralor <prithoo11335@gmail.com>
2023-05-20 15:07:00 +0530

    unique trait

--- src/libs/dsa.rs
+++ src/libs/dsa.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 
 pub fn factorial(num: u64) -> u64 {
     if num == 1 as u64 {
@@ -5,5 +7,20 @@ pub fn factorial(num: u64) -> u64 {
     }
     else {
         return num*factorial(num-1);
+    }
+}
+
+// This trait allows a collection of sequence of values check all unique elements.
+pub trait UniqueElements<T> {
+    fn unique(&self) -> Vec<T>;
+}
+
+/// Implementation of UniqueElements for a Vector of datatype `T`.
+impl<T: Eq + std::hash::Hash + Clone + Ord> UniqueElements<T> for Vec<T> {
+    fn unique(&self) -> Vec<T> {
+        let set: HashSet<_> = self.iter().cloned().collect();
+        let mut res: Vec<T> = set.into_iter().collect();
+        res.sort();
+        return res;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![allow(warnings)]
+
+use libs::dsa::UniqueElements;
 mod libs;
 
 fn main() {
     // libs::try_prime_checker::try_prime_checker();
-    let num: u64 = 6;
-    let fac: u64 = libs::dsa::factorial(num);
-    println!("The factorial of {} is:\t{}", num, fac);
+    // let num: u64 = 6;
+    // let fac: u64 = libs::dsa::factorial(num);
+    // println!("The factorial of {} is:\t{}", num, fac);
+
+    let _list: Vec<u64> = vec![1, 9, 9, 2, 0, 9, 2, 5];
+    let unique_elements = _list.unique();
+    println!("{:?}", unique_elements);
 }
 
