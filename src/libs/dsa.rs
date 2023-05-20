95f4aca293e3743027533a96c4bb1d017f93a433
Arkiralor <prithoo11335@gmail.com>
2023-05-20 15:15:18 +0530

    docs

--- src/libs/dsa.rs
+++ src/libs/dsa.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 
 pub fn factorial(num: u64) -> u64 {
+    //! Function to find the factorial of a given unsigned, 64-bit number.
     if num == 1 as u64 {
         return 1;
     }
